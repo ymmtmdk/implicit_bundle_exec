79e60db8e7cd532ad8a2fc37e43e0c751f9faa26
ymmtmdk <ymmtmdk@gmail.com>
2017-05-06 01:14:14 +0900

    no comment

--- init.fish
+++ init.fish
@@ -1,2 +1,14 @@
+function __wrap_bundle_exec_binaries --on-variable implicit_bundle_exec_binaries
+  for e in $implicit_bundle_exec_binaries
+    function $e --inherit-variable e
+      if file_exists_in_tree Gemfile
+        command bundle exec $e $argv
+      else
+        eval command $e $argv
+      end
+    end
+  end
+end
+
 set -g implicit_bundle_exec_binaries rails guard rackup spec
 
